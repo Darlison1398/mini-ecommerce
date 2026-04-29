abad8ab0eda7ce2c9e47ba81b9b960480d86b10e
Darlison Silva <drsilva123jd@gmail.com>
2026-04-28 21:00:08 -0300

    ajustado navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -23,61 +23,74 @@ export const Navbar = () => {
       </Link>
 
       <div className="flex items-center gap-4">
-        <Link
-          to="/"
-          className="flex items-center gap-1 text-gray-600 dark:text-gray-300 hover:text-blue-600 transition"
-        >
-          <House size={18}/>
-          <span className="hidden sm:block">Home</span>
-        </Link>
 
-        {user?.role === "client" && (
-          <Link
-            to="/orders"
-            className="flex items-center gap-1 text-gray-600 dark:text-gray-300 hover:text-blue-600 transition"
-          >
-            <Package size={18} />
-            <span className="hidden sm:block">Pedidos</span>
-          </Link>
-        )}
+        {user && (
+          <>
+            <Link
+              to="/"
+              className="flex items-center gap-1 text-gray-600 dark:text-gray-300 hover:text-blue-600 transition"
+            >
+              <House size={18}/>
+              <span className="hidden sm:block">Home</span>
+            </Link>
 
-        {user?.role === "admin" && (
-          <Link 
-            to="/admin/users"
-            className="flex items-center gap-1 text-gray-600 dark:text-gray-300 hover:text-blue-600 transition"
-          >
-            <Users size={18}/>
-            <span className="hidden sm:block">Usuários</span>
-          </Link>
-        )}
-
-        {user?.role === "client" && (
-          <Link
-            to="/cart"
-            className="relative p-2 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800 transition"
-          >
-            <ShoppingCart
-              className={`text-gray-700 dark:text-gray-200 transition-transform ${
-                isAnimating ? "animate-bounce" : ""
-              }`}
-            />
-
-            {totalItems > 0 && (
-              <span className="absolute -top-1 -right-1 bg-red-500 text-white text-[10px] font-semibold px-1.5 py-0.5 rounded-full shadow">
-                {totalItems}
-              </span>
+            {user.role === "client" && (
+              <Link
+                to="/orders"
+                className="flex items-center gap-1 text-gray-600 dark:text-gray-300 hover:text-blue-600 transition"
+              >
+                <Package size={18} />
+                <span className="hidden sm:block">Pedidos</span>
+              </Link>
             )}
-          </Link>
-        )}
 
-        {user?.role === "admin" && (
-          <Link 
-            to="/admin/products"
-            className="flex items-center gap-1 text-gray-600 dark:text-gray-300 hover:text-blue-600 transition"
-          >
-            <SquareChartGantt  size={18}/>
-            <span className="hidden sm:block">Produtos</span>
-          </Link>
+            {user.role === "admin" && (
+              <Link 
+                to="/admin/users"
+                className="flex items-center gap-1 text-gray-600 dark:text-gray-300 hover:text-blue-600 transition"
+              >
+                <Users size={18}/>
+                <span className="hidden sm:block">Usuários</span>
+              </Link>
+            )}
+
+            {user.role === "client" && (
+              <Link
+                to="/cart"
+                className="relative p-2 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800 transition"
+              >
+                <ShoppingCart
+                  className={`text-gray-700 dark:text-gray-200 transition-transform ${
+                    isAnimating ? "animate-bounce" : ""
+                  }`}
+                />
+
+                {totalItems > 0 && (
+                  <span className="absolute -top-1 -right-1 bg-red-500 text-white text-[10px] font-semibold px-1.5 py-0.5 rounded-full shadow">
+                    {totalItems}
+                  </span>
+                )}
+              </Link>
+            )}
+
+            {user.role === "admin" && (
+              <Link 
+                to="/admin/products"
+                className="flex items-center gap-1 text-gray-600 dark:text-gray-300 hover:text-blue-600 transition"
+              >
+                <SquareChartGantt size={18}/>
+                <span className="hidden sm:block">Produtos</span>
+              </Link>
+            )}
+
+            <button
+              onClick={logout}
+              className="flex items-center gap-1 bg-red-500 hover:bg-red-600 text-white px-3 py-2 rounded-lg transition text-sm"
+            >
+              <LogOut size={16} />
+              <span className="hidden sm:block">Sair</span>
+            </button>
+          </>
         )}
 
         <button
@@ -87,14 +100,8 @@ export const Navbar = () => {
           {theme === "dark" ? <Moon size={18} /> : <Sun size={18} />}
         </button>
 
-        <button
-          onClick={logout}
-          className="flex items-center gap-1 bg-red-500 hover:bg-red-600 text-white px-3 py-2 rounded-lg transition text-sm"
-        >
-          <LogOut size={16} />
-          <span className="hidden sm:block">Sair</span>
-        </button>
       </div>
+
     </nav>
   );
 };
